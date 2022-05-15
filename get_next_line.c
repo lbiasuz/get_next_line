7832f0991fadd859f0df7bc03e18328b9c618cf2
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-05-15 13:26:06 -0300

    Ultima linha retornando duplicada

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,55 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 20:44:05 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/05/14 21:48:22 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/05/14 23:31:31 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*gen_line(char **src)
+char	*gen_line(char **s_src)
 {
 	char	*nlp;
 	char	*temp;
 	
-	nlp = ft_strchr(src, '\n');
+	nlp = ft_strchr(*s_src, '\n');
 	if (nlp)
 	{
-		temp = ft_substr(*src, 0, nlp - *src + 1);
-		*src = nlp + 1;
+		temp = ft_substr(*s_src, 0, nlp - *s_src + 1);
+		*s_src = nlp + 1;
 		return (temp);
 	}
-	return (*src);
+	return (*s_src);
+}
+
+char	*read_file(int fd)
+{
+	char	*temp;
+	int		n_read;
+
+	temp = malloc((sizeof(char) * BUFFER_SIZE) + 1);
+	n_read = read(fd, temp, BUFFER_SIZE);
+	if (n_read < 1)
+		return ("");
+	temp[n_read] = 0;
+	return (temp);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*the_string;
-	char		*line;
-	// char		temp[BUFFER_SIZE + 1];
-	// int			n_read;
+	char	*temp;
 
+	if (!BUFFER_SIZE || fd < 0)
+		return (NULL);
 	if (!the_string)
+		the_string = read_file(fd);
+	while (!ft_strchr(&*the_string, '\n'))
 	{
-		the_string = malloc(BUFFER_SIZE + 1);
-		read(fd, the_string, BUFFER_SIZE);
-		the_string[BUFFER_SIZE] = '\0';
+		temp = read_file(fd);
+		if (ft_strlen(temp) < BUFFER_SIZE)
+			return (the_string);
+		the_string = ft_strjoin(&*the_string, temp);
 	}
-	// while (!ft_strchr())
-	line = gen_line(&the_string);
-	// ft_strlcpy(the_string, ft_strchr(the_string, '\n', )
-	return (line);
+	return (gen_line(&the_string));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 20:44:07 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/05/14 20:25:47 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/05/14 22:21:49 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 char	*gen_line(char **src);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strchr(char **src, char c);
+char	*ft_strchr(char *src, char c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *dest, char const *src);
 char	*get_next_line(int fd);
